072b22494e7e4b146ef3d8559e3eb371111e3367
Marcus Botacin <mfbotacin@Inf.ufpr.br>
2024-06-03 17:55:44 -0300

    skeleton

---
+++ main.py
@@ -0,0 +1,14 @@
+# we import the new framework
+from capymoasec.rule_generators import autoyara
+
+# We instantiate our autoyara representation
+autoY = autoyara.AutoYara(params=[])
+# we train with the reference goodware
+autoY.train([])
+# we generate the rules for the candidate files
+autoY.generate([])
+
+# we here can instantiate a different generator
+# from capymoasec.rule_generator import another_generator
+# another_generator.gen()
+# compare results from autoyara with the other generator
